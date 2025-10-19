91a3c6e8167944c502a877086835c873235b9cac
IsaacCodes <baconboyxy@deter.com>
2025-10-18 19:58:09 -0700

    Put game screen in class, switch to camel case, other minor changes. Next need to make settled, piece spawn, + collision logic

--- ece5-tetris.ino
+++ ece5-tetris.ino
@@ -1,7 +1,7 @@
 //Game grid info
-const uint8_t game_grid_height = 20; //Should be 40 for the real game
-const uint8_t game_grid_width = 10;
-int game_grid[game_grid_height][game_grid_width] = {0};
+const uint8_t gameGridHeight = 20; //TODO: Should be 40 for the real game
+const uint8_t gameGridWidth = 10;
+int gameGrid[gameGridHeight][gameGridWidth] = {0};
 
 //Enum for piece types
 enum PieceType {
@@ -27,91 +27,127 @@ const uint8_t pieceDesign[NUM_PIECES][3][3] = {
 };
 
 //GamePiece class
-class GamePiece {
+class Piece {
   private:
     //Piece type + pos
     PieceType type;
-    uint8_t x, y;
+    //x, y relative to top left
+    uint8_t x, y, height;
+    bool settled;
 
   public:
-    //Constructor, pawns piece at center
-    GamePiece() {
+    //Piece constructor
+    Piece() {
       type = static_cast<PieceType>(random(NUM_PIECES));
+
+      //Finds the bottom of the piece (used by fall logic)
+      for (int i = 2; i >= 0; i--) {
+        for (int j = 0; j < 3; j++) {
+          if (pieceDesign[type][i][j] == 1) {
+            height = y + i;
+            goto breakLoops; //break from both loops
+          }
+        }
+      } 
+      //Used by goto
+      breakLoops:
+      //Always start at top center
       x = 3; y = 0;
+      //Whether the piece should stop falling
+      settled = false;
     }
 
     //Empties the section of the game_grid a piece occupies
-    void zeroPiece() {
+    void erase() {
       for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-          game_grid[y + i][x + j] = 0;
+          gameGrid[y + i][x + j] = 0;
         }
       }
     }
 
     //Temp print to Serial
-    void printPiece() {
+    void print() {
       for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-          game_grid[y + i][x + j] = pieceDesign[type][i][j];
+          gameGrid[y + i][x + j] = pieceDesign[type][i][j];
         }
       }
     }
 
     //Simple gravity, can add collision later
     void fall() {
-      if (y < game_grid_height - 3) { //Minus 3 since it is of size 3, need to make this much better in the future
-        zeroPiece();
-        y++;
-        printPiece();
+      //If at the bottom of the screen, set it to settled
+      if (y + height == gameGridHeight - 1) {
+        settled = true;
+        return;
       }
+      //Need to write code to set it to settled if there is a block below
+
+      //Otherwise fall
+      erase();
+      y++;
+      print();
     }
 };
 
 
+//GameScreen class, mostly temporary atm (will need to be updated with real screen functions)
+class GameScreen {
+  private:
+    //Temporary screen management code (mmmm thats eating a lot of ram)
+    char buffer[gameGridHeight * (gameGridWidth+1) + 1]; //40 lines * (10 blocks + 1 '\n') + '\0'
 
-//Temporary screen management code (mmmm thats eating a lot of ram)
-char game_screen[game_grid_height * (game_grid_width+1) + 1]; //40 lines * (10 blocks + 1 '\n') + '\0'
-void update_screen() {
-  uint16_t game_screen_i = 0;
-  for(uint8_t y = 0; y < game_grid_height; y++) {
-    for(uint8_t x = 0; x < game_grid_width; x++) {
-      game_screen[game_screen_i] = '0' + game_grid[y][x]; //Convert it to a char and put it on the screen string
-      game_screen_i++;
+  public:
+    //gameScreen constructor
+    GameScreen() {
+      //Nothing atm
     }
-    game_screen[game_screen_i] = '\n';
-    game_screen_i++;
-  }
-  game_screen[game_screen_i] = '\0';
 
-  Serial.println(game_screen);
-}
+    void updateScreen() {
+      //Loop through game screen, keeping track of index
+      uint16_t i = 0;
+      for(uint8_t y = 0; y < gameGridHeight; y++) {
+        for(uint8_t x = 0; x < gameGridWidth; x++) {
+          buffer[i] = '0' + gameGrid[y][x]; //Convert it to a char and put it on the screen string
+          i++;
+        }
+        buffer[i] = '\n';
+        i++;
+      }
+      buffer[i] = '\0';
+
+      Serial.println(buffer);
+    }
+};
 
 
 //Init
-GamePiece gp0;
+Piece activePiece;
+GameScreen screen;
+
 void setup() {
   Serial.begin(115200);
   Serial.println("\n--------------------\n");
-  gp0 = GamePiece();
+  //Init classes
+  activePiece = Piece();
+  screen = GameScreen();
 }
 
 
 //Used to manage time in the game loop
 //Time in ms between ticks
-const uint16_t tick_time = 1000;
-uint16_t last_tick = 0;
+const uint16_t tickTime = 1000;
+uint16_t lastTick = 0;
 
 //Main game loop
 void loop() {
-
   //Every tick_time ms, fall + update the screen
-  if (millis() - last_tick >= tick_time) {
-    //Make pieces fall + update screen
-    gp0.fall();
-    update_screen();
+  if (millis() - lastTick >= tickTime) {
+    //Make active game piece fall + update screen
+    activePiece.fall();
+    screen.updateScreen();
 
-    last_tick = millis();
-
+    lastTick = millis();
   }
 }

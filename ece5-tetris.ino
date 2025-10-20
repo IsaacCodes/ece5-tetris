44caaa4e6a5479064f2f20d8491292baf8ac4fe3
IsaacCodes <baconboyxy@deter.com>
2025-10-19 18:04:35 -0700

    Made it so new pieces spawn when an old piece becomes settled. Added collisions to fall function. Fixed random not working. Other minor bugsfixes/changes.

--- ece5-tetris.ino
+++ ece5-tetris.ino
@@ -1,4 +1,4 @@
-//Game grid info
+//Game grid info, modified by the active Piece and read by the GameIO
 const uint8_t gameGridHeight = 20; //TODO: Should be 40 for the real game
 const uint8_t gameGridWidth = 10;
 int gameGrid[gameGridHeight][gameGridWidth] = {0};
@@ -10,7 +10,7 @@ enum PieceType {
   NUM_PIECES
 };
 
-// Define the static piece designs
+//Defines the piece designs
 const uint8_t pieceDesign[NUM_PIECES][3][3] = {
   //L
   {
@@ -26,7 +26,7 @@ const uint8_t pieceDesign[NUM_PIECES][3][3] = {
   }
 };
 
-//GamePiece class
+//Piece class to manage active piece movement
 class Piece {
   private:
     //Piece type + pos
@@ -38,6 +38,11 @@ class Piece {
   public:
     //Piece constructor
     Piece() {
+      //Always start at top center
+      x = 3; y = 0;
+      //Whether the piece should stop falling
+      settled = false;
+      //Sets a random piece type
       type = static_cast<PieceType>(random(NUM_PIECES));
 
       //Finds the bottom of the piece (used by fall logic)
@@ -51,24 +56,22 @@ class Piece {
       } 
       //Used by goto
       breakLoops:
-      //Always start at top center
-      x = 3; y = 0;
-      //Whether the piece should stop falling
-      settled = false;
+      //Nothing atm. TODO: delete if nothing on final product
+      return;
     }
 
     //Empties the section of the game_grid a piece occupies
-    void erase() {
-      for (int i = 0; i < 3; i++) {
+    void erase() const {
+      for (int i = 0; i <= height; i++) { // < height to prevent erasing lower blocks
         for (int j = 0; j < 3; j++) {
           gameGrid[y + i][x + j] = 0;
         }
       }
     }
 
-    //Temp print to Serial
-    void print() {
-      for (int i = 0; i < 3; i++) {
+    //Print to the gameGrid, which is then handlded by IO
+    void print() const {
+      for (int i = 0; i <= height; i++) {
         for (int j = 0; j < 3; j++) {
           gameGrid[y + i][x + j] = pieceDesign[type][i][j];
         }
@@ -82,28 +85,40 @@ class Piece {
         settled = true;
         return;
       }
-      //Need to write code to set it to settled if there is a block below
+      //Checks if there is a block below anywhere
+      for (int j = 0; j < 3; j++) {
+        if (gameGrid[y + height + 1][x + j] == 1) {
+          settled = true;
+          return;
+        }
+      }
 
       //Otherwise fall
       erase();
       y++;
       print();
     }
+
+    //Returns settled state
+    bool isSettled() const {
+      return settled;
+    }
 };
 
 
-//GameScreen class, mostly temporary atm (will need to be updated with real screen functions)
-class GameScreen {
+//GameIO class, mostly temporary atm (will need to be updated with real screen and input functions)
+class GameIO {
   private:
     //Temporary screen management code (mmmm thats eating a lot of ram)
     char buffer[gameGridHeight * (gameGridWidth+1) + 1]; //40 lines * (10 blocks + 1 '\n') + '\0'
 
   public:
-    //gameScreen constructor
-    GameScreen() {
+    //GameIO constructor
+    GameIO() {
       //Nothing atm
     }
 
+    //Function to update the screen (using buffer atm)
     void updateScreen() {
       //Loop through game screen, keeping track of index
       uint16_t i = 0;
@@ -122,22 +137,26 @@ class GameScreen {
 };
 
 
-//Init
+//Global vars
 Piece activePiece;
-GameScreen screen;
-
+GameIO gameIO;
+//Runs once on startup
 void setup() {
+  //Init Serial
   Serial.begin(115200);
   Serial.println("\n--------------------\n");
+  //Init random
+  //NOTE: If we ever connect something to A0, this might need to be changed
+  randomSeed(analogRead(A0));
   //Init classes
   activePiece = Piece();
-  screen = GameScreen();
+  gameIO = GameIO();
 }
 
 
 //Used to manage time in the game loop
 //Time in ms between ticks
-const uint16_t tickTime = 1000;
+const uint16_t tickTime = 200;
 uint16_t lastTick = 0;
 
 //Main game loop
@@ -146,8 +165,16 @@ void loop() {
   if (millis() - lastTick >= tickTime) {
     //Make active game piece fall + update screen
     activePiece.fall();
-    screen.updateScreen();
+    gameIO.updateScreen();
 
     lastTick = millis();
   }
+
+  if (activePiece.isSettled()) {
+    //Create a new piece
+    Serial.println("New piece");
+    activePiece = Piece();
+  }
+
+  //TODO: Need code for when a new block must spawn (after old block is settled)
 }

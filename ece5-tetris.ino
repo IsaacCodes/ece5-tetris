af483ec021ecdb7e73b53f58212583e3463dc26a
IsaacCodes <baconboyxy@deter.com>
2025-10-31 11:04:59 -0700

    Tested on real hardware. Redid collision logic so it works properly for both boundary and piece collisions. Added buzzer code.

--- ece5-tetris.ino
+++ ece5-tetris.ino
@@ -1,13 +1,16 @@
 //Game grid info
-constexpr uint8_t gameHeight = 20, gameWidth = 10; //TODO: Should be height 40 for the real game
+constexpr uint8_t gameHeight = 20, gameWidth = 10;
 bool gameGrid[gameHeight][gameWidth] = {0};
 
-/* EOF? */
+/*
+  PIECE CODE
+*/
 
 //Enum for piece types
 enum PieceType {
   L,
   T,
+  O,
   NUM_PIECES
 };
 
@@ -15,205 +18,211 @@ enum PieceType {
 constexpr uint8_t pieceDesign[NUM_PIECES][3][3] = {
   //L
   {
-    {1, 0, 0},
-    {1, 1, 1},
-    {0, 0, 0}
-  },
+    { 1, 0, 0 },
+    { 1, 1, 1 },
+    { 0, 0, 0 } },
   //T
   {
-    {0, 1, 0},
-    {1, 1, 1},
-    {0, 0, 0}
-  }
+    { 0, 1, 0 },
+    { 1, 1, 1 },
+    { 0, 0, 0 } },
+  //O
+  {
+    { 1, 1, 0 },
+    { 1, 1, 0 },
+    { 0, 0, 0 } }
 };
 
 //Piece class to manage active piece movement
 class Piece {
-  private:
-    //Piece type + pos  
-    PieceType type;
-    //x, y relative to top left
-    uint8_t x, y, height;
-    bool settled;
+private:
+  //Piece type + pos
+  PieceType type;
+  //x, y relative to top left
+  uint8_t x, y;
+  bool settled;
 
-  public:
-    //Piece constructor
-    Piece() {
-      //Always start at top center
-      x = 3; y = 0;
-      //Whether the piece should stop falling
-      settled = false;
-      //Sets a random piece type
-      type = static_cast<PieceType>(random(NUM_PIECES));
+public:
+  //Piece constructor
+  Piece() {
+    //Always start at top center
+    x = 3;
+    y = 0;
+    //Whether the piece should stop falling
+    settled = false;
+    //Sets a random piece type
+    type = static_cast<PieceType>(random(NUM_PIECES));
+  }
 
-      //Finds the bottom of the piece (used by fall logic)
-      for (int i = 2; i >= 0; i--) {
-        for (int j = 0; j < 3; j++) {
-          if (pieceDesign[type][i][j] == 1) {
-            height = y + i;
-            goto breakLoops; //break from both loops
-          }
-        }
-      } 
-      //Used by goto
-      breakLoops:
-      //Nothing atm. TODO: delete if nothing on final product
-      return;
-    }
-
-    //Empties the section of the game_grid a piece occupies
-    void erase() const {
-      for (int i = 0; i <= height; i++) { // < height to prevent erasing lower blocks
-        for (int j = 0; j < 3; j++) {
+  //Empties the section of the gameGrid a piece occupies
+  void erase() const {
+    for (int8_t i = 0; i < 3; i++) {
+      for (int8_t j = 0; j < 3; j++) {
+        if (pieceDesign[type][i][j] == 1) {
           gameGrid[y + i][x + j] = 0;
         }
       }
     }
+  }
 
-    //Print to the gameGrid, which is then handlded by IO
-    void print() const {
-      for (int i = 0; i <= height; i++) {
-        for (int j = 0; j < 3; j++) {
-          gameGrid[y + i][x + j] = pieceDesign[type][i][j];
+  //Print to the gameGrid, which is then handled by gameIO
+  void print() const {
+    for (int8_t i = 0; i < 3; i++) {
+      for (int8_t j = 0; j < 3; j++) {
+        if (pieceDesign[type][i][j] == 1) {
+          gameGrid[y + i][x + j] = 1;
         }
       }
     }
+  }
 
-    //Gravity to make the piece fall (natural or when pressing down)
-    void fall() {
-      //If at the bottom of the screen, set it to settled
-      if (settled || y + height == gameHeight - 1) {
-        settled = true;
-        return;
+  bool willCollide(int8_t dy, int8_t dx) {
+    //Loops thru each position of the block
+    for (int8_t i = 0; i < 3; i++) {
+      for (int8_t j = 0; j < 3; j++) {
+        //If position is out of range or occupied, return true
+        int8_t ny = y + i + dy, nx = x + j + dx;
+        if (pieceDesign[type][i][j] == 1 && (ny >= gameHeight || nx < 0 || nx >= gameWidth || gameGrid[ny][nx] == 1)) return true;
       }
-      //If there is a block below anywhere, set to settled
-      for (int j = 0; j < 3; j++) {
-        if (gameGrid[y + height + 1][x + j] == 1) {
-          settled = true;
-          return;
-        }
-      }
-
-      //Otherwise fall
-      erase();
-      y++;
-      print();
     }
+    //Otherwise return false
+    return false;
+  }
 
-    void moveLeft() {
-      //If at the edge of the screen, don't move
-      if (x - 1 == 0) return;
-      
-      //TODO: Need code for collision check for blocks on the sides
+  //Gravity to make the piece fall (natural or when pressing down)
+  void fall() {
+    //Erase the current position
+    erase();
+    //If spot is open, go down
+    if (!willCollide(1, 0)) y++;
+    //Otherwise set to settled
+    else settled = true;
+    //Then reprint
+    print();
+  }
 
-      //Otherwise move
-      erase();
-      x--;
-      print();
-    }
+  void moveLeft() {
+    //Erase the current position
+    erase();
+    //If spot is open, go left
+    if (!willCollide(0, -1)) x--;
+    //Then reprint
+    print();
+  }
 
-    void moveRight() {
-      //If at the edge of the screen, don't move
-      if (x + 1 == gameWidth - 1) return;
-      
-      //TODO: Need code for collision check for blocks on the sides
+  void moveRight() {
+    //Erase the current position
+    erase();
+    //If spot is open, go left
+    if (!willCollide(0, 1)) x++;
+    //Then reprint
+    print();
+  }
 
-      //Otherwise move
-      erase();
-      x++;
-      print();
-    }
-
-    //Returns settled state
-    bool isSettled() const {
-      return settled;
-    }
+  //Returns settled state
+  bool isSettled() const {
+    return settled;
+  }
 };
 
-/* EOF */
+/*
+  GAME IO CODE
+*/
 
 //GameInput class, handles button inputs (will need to update pins but otherwise should be the same)
 class GameInput {
-  private:
-    //Button input pins
-    static constexpr uint8_t leftPin = 12, downPin = 11, rightPin = 10, rotatePin = 9;
+private:
+  //Button input pins
+  static constexpr uint8_t leftPin = 4, rightPin = 3, downPin = 11, rotatePin = 9;
 
-  public:
-    //GameInput constructor, sets pin modes
-    GameInput() {
-      //Sets pinModes
-      pinMode(leftPin, INPUT);
-      pinMode(downPin, INPUT);
-      pinMode(rightPin, INPUT);
-      pinMode(rotatePin, INPUT);
+public:
+  //GameInput constructor, sets pin modes
+  GameInput() {
+    //Sets pinModes
+    pinMode(leftPin, INPUT);
+    pinMode(rightPin, INPUT);
+    pinMode(downPin, INPUT);
+    pinMode(rotatePin, INPUT);
+  }
+
+  bool readInput(Piece &activePiece) {
+    if (digitalRead(leftPin) == HIGH) {
+      activePiece.moveLeft();
+      Serial.println("left");
+      return true;
+    }
+    if (digitalRead(rightPin) == HIGH) {
+      activePiece.moveRight();
+      Serial.println("right");
+      return true;
     }
 
-    bool readInput( Piece &activePiece) {
-      if(digitalRead(leftPin) == HIGH) {
-        activePiece.moveLeft();
-        return true;
-      }
-      if(digitalRead(downPin) == HIGH) {
-        activePiece.fall();
-        return true;
-      }
-      if(digitalRead(rightPin) == HIGH) {
-        activePiece.moveRight();
-        return true;
-      }
-      if(digitalRead(rotatePin) == HIGH) {
-        //TODO: handle rotate input
-      }
-      
-      return false;
+    return false;
+
+    if (digitalRead(downPin) == HIGH) {
+      activePiece.fall(); //TODO: Check if this works
+      return true;
     }
+    if (digitalRead(rotatePin) == HIGH) {
+      //TODO: handle rotate input
+    }
+
+    return false;
+  }
 };
 
 //GameOutput class, mostly temporary atm (will need to be updated with real screen functions)
 class GameOutput {
-  private:
-    //Temporary screen management code (mmmm thats eating a lot of ram)
-    char buffer[gameHeight * (gameWidth+1) + 1]; //40 lines * (10 blocks + 1 '\n') + '\0'
+private:
+  //Temporary screen management code (mmmm thats eating a lot of ram)
+  char buffer[gameHeight * (gameWidth + 1) + 1];  //40 lines * (10 blocks + 1 '\n') + '\0'
 
-  public:
-    //GameOutput constructor
-    GameOutput() {
-      //Nothing atm
-    }
+public:
+  //GameOutput constructor
+  GameOutput() {
+    //Nothing atm
+  }
 
-    //Function to update the screen (using buffer atm)
-    void updateScreen() {
-      //Loop through game screen, keeping track of index
-      uint16_t i = 0;
-      for(uint8_t y = 0; y < gameHeight; y++) {
-        for(uint8_t x = 0; x < gameWidth; x++) {
-          buffer[i] = '0' + gameGrid[y][x]; //Convert it to a char and put it on the screen string
-          i++;
+  //Function to update the screen (using buffer atm)
+  void updateScreen() {
+    //Loop through game screen, keeping track of index
+    uint16_t i = 0;
+    for (uint8_t y = 0; y < gameHeight; y++) {
+      for (uint8_t x = 0; x < gameWidth; x++) {
+        if (gameGrid[y][x]) {
+          buffer[i] = '#';
+        } else {
+          buffer[i] = '.';
         }
-        buffer[i] = '\n';
         i++;
       }
-      buffer[i] = '\0';
-
-      Serial.println(buffer);
+      buffer[i] = '\n';
+      i++;
     }
+    buffer[i] = '\0';
 
+    Serial.println(buffer);
+  }
 };
 
-/* EOF */
+/*
+  SETUP + LOOP CODE
+*/
 
 //Global vars
+constexpr int8_t buzzerPin = 2;
 Piece activePiece;
 GameInput gameInput;
 GameOutput gameOutput;
 //Runs once on startup
 void setup() {
   //Init Serial
-  Serial.begin(115200);
+  Serial.begin(500000);
   Serial.println("\n--------------------\n");
   //Init random
-  //NOTE: If we ever connect something to A0, this might need to be changed
   randomSeed(analogRead(A0));
+  //Init buzzer
+  pinMode(buzzerPin, OUTPUT);
   //Init classes
   activePiece = Piece();
   gameInput = GameInput();
@@ -223,9 +232,9 @@ void setup() {
 
 //Used to manage time in the game loop (TODO: maybe move these to respective class later, idk)
 //Time in ms between ticks
-constexpr uint16_t tickTime = 200;
+constexpr uint16_t tickTime = 500;
 uint16_t lastTick = 0;
-constexpr uint16_t inputTime = 100;
+constexpr uint16_t inputTime = 500;
 uint16_t lastInput = 0;
 
 //Main game loop
@@ -233,13 +242,14 @@ void loop() {
   bool screenChanged = false;
 
   //Get user input (has a cooldown of inputTime)
-  if (millis() - lastInput >= inputTime && gameInput.readInput(activePiece)) {
+  if ((millis() - lastInput >= inputTime) && gameInput.readInput(activePiece)) {
     screenChanged = true;
     lastInput = millis();
   }
 
   //Make active game piece fall + update screen (cooldown of tickTime)
   if (millis() - lastTick >= tickTime) {
+    screenChanged = true;
     activePiece.fall();
     lastTick = millis();
   }
@@ -249,9 +259,10 @@ void loop() {
     //Create a new piece
     Serial.println("New piece");
     activePiece = Piece();
+    tone(buzzerPin, 500);
+    delay(500);
+    noTone(buzzerPin);
   }
 
-  if(screenChanged) gameOutput.updateScreen();
+  if (screenChanged) gameOutput.updateScreen();
 }
-
-//TODO: Implement scoring, finish piece movement code
